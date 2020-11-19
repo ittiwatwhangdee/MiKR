1cd90db219f41d4dadfe0694315a40b5b52363a8
fhutzchx <fahut.ittiwat@gmail.com>
2020-11-20 00:20:37 +0700

    edit

--- js/app.js
+++ js/app.js
@@ -1,42 +1,39 @@
-document.addEventListener('init', function(event) {
-    var page = event.target;
-  
-    if (page.id === 'home') {
-      page.querySelector('#menu_button').onclick = function() {
-        document.querySelector('#menu').open();
-      };
-  
-      //The postchange event listener is used for changing the dots when the carousel is changed
-  
-      page.querySelector('#carousel').addEventListener("postchange", function() {
-        page.querySelector('#dot0').classList.remove("circle_current");
-        page.querySelector('#dot1').classList.remove("circle_current");
-        page.querySelector('#dot2').classList.remove("circle_current");
-  
-        page.querySelector('#dot' + page.querySelector('#carousel').getActiveIndex()).classList.add("circle_current");
-  
-      });
-    } else if (page.id === 'movie_details') {
-      page.querySelector('#movie_title').innerHTML = page.data.title;
-    }
-  });
-  
-  function openMovieDetails(id) {
+$(function() {
+    document.addEventListener('init', function(event) {
+        var page = event.target;
+
+        console.log(page.id);
+        if (page.id === 'Home') {
+            $('#back').hide();
+            page.querySelector('#johnwick').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail.html');
+
+            };
+            page.querySelector('#wonderwoman').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail2.html');
+            };
+            page.querySelector('#toystory').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/detail3.html');
+            };
+        } else if (page.id === 'johnwick' || page.id === 'wonderwoman' || page.id === 'toystory') {
+            $('#back').show();
+            document.querySelector('ons-back-button').onclick = function(event) {
+                document.querySelector('#myNavigator').popPage();
+            };
+        } 
+    });
+    
+})
+
+function openMovieDetails(id) {
     document.querySelector('#myNavigator').pushPage('movie_details.html', {data: {title: id}});
   }
-  
-  function openHome() {
-    document.querySelector('#myNavigator').pushPage('home_splitter.html');
-  }
+
   function goBack() {
     document.querySelector('#menu').close().then(function() {
       document.querySelector('#myNavigator').popPage()
     });
   }
 
-  
 
-
-  
-  
 
