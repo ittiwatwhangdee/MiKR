e9b8cd24c783c7f0a67cada0c7b48ccf7b56e91f
fhutzchx <fahut.ittiwat@gmail.com>
2020-12-01 00:06:50 +0700

    edit

--- js/app.js
+++ js/app.js
@@ -66,4 +66,79 @@ function openproductDetails(id) {
       .then(menu.close.bind(menu));
   };
 
- 
+
+
+  
+  $("#search").click(function () {
+    console.log("1");
+    
+           
+    $("#search_show").empty();
+    $("#sug_show").empty();
+    $("#search_show").append("");
+    var search_input = document.getElementById("search_input").value;
+    console.log(search_input);
+  
+    db.collection("search").get().then((querySnapshot) => {
+  
+      querySnapshot.forEach((doc) => {
+  
+        var nameforcheck = `${doc.data().name}`;
+        // console.log(titleforcheck);
+        var priceforcheck = `${doc.data().price}`;
+        // console.log(yearforcheck);
+  
+        var regexNumber = /\d/;
+        var regexLetter = /[a-zA-z]/;
+  
+        if (regexLetter.test(search_input)) {
+  
+          if (nameforcheck.indexOf(search_input) != -1) {
+            var row = `
+            <ons-row style="margin: 5px;">
+            <ons-col class="text-center">
+            <img src="${doc.data().posterURL}" width="70%" style="margin: 5px 5px;" alt="">
+            </ons-col>
+    
+            <ons-col>
+            <p style="font-size:18px">${doc.data().name} (${doc.data().price}) </p>
+            <p style="font-size:15px">${doc.data().entertainment}</p>
+            <p style="font-size:15px">`+ rating +`</p>
+            </ons-col>
+          
+            </ons-row>`
+  
+  
+          }
+  
+  
+        } else if (regexNumber.test(search_input)) {
+  
+          if (search_input == priceforcheck) {
+            var row = `
+            <ons-row style="margin: 5px;">
+            <ons-col class="text-center">
+            <img src="${doc.data().posterURL}" width="80%" style="margin: 5px 5px;" alt="">
+            </ons-col>
+    
+            <ons-col>
+            <p style="font-size:10px">${doc.data().name} (${doc.data().price}) (${doc.data().price})</p>
+            </ons-col>
+          
+            </ons-row>`
+  
+          }
+     
+        } else {
+          $("#search_show").empty();
+          $("#search_show").append("");
+  
+        }
+  
+        $("#search_show").append(row);
+  
+      });
+  
+    });
+  
+  });
