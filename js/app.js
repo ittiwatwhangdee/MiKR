17c2f55dc92c015ef084f07fce5b4fab58d86b3b
fhutzchx <fahut.ittiwat@gmail.com>
2020-12-04 01:15:27 +0700

    edit

--- js/app.js
+++ js/app.js
@@ -105,11 +105,11 @@ function openproductDetails(id) {
         <ons-row class="rowmagin se" id="${doc.data().name}"> 
         <ons-row style="margin: 5px;">
             <ons-col class="text-center">
-            <img src="${doc.data().poster}" width="80%" style="margin: 5px 5px;" alt="">
+            <img src="${doc.data().poster}" width="75%" style="margin: 5px 5px;" alt="">
             </ons-col>
         <ons-col>
             <p style="font-size:18px">${doc.data().name} (${doc.data().years}) </p>
-            <p style="font-size:15px">${doc.data().plot}</p>
+            <p class="detail_search">${doc.data().plot}</p>
             <p style="font-size:15px">`+ star +`</p>
             </ons-col>
       </ons-row>`
@@ -125,108 +125,6 @@ function openproductDetails(id) {
 
 }
 
-// function getfromSearch() {
-           
-//             $("#search").click(function () {
-//            $("#search_show").empty();
-//            $("#sug_show").empty();
-//            $("#search_show").append("");
-//            var search_input = document.getElementById("search_input").value;
-//            console.log(search_input);
-         
-//            db.collection("search").get().then((querySnapshot) => {
-         
-//              querySnapshot.forEach((doc) => {
-//             if (`${doc.data().star}`>4){
-//                 var star = `
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
-//             }else if(`${doc.data().star}`>3){
-//                 var star = `
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
-//             }else if(`${doc.data().star}`>2){
-//                 var star = `
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
-//             }else if(`${doc.data().star}`>1){
-//                 var star = `
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
-//             }else{
-//                 var star = `
-//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
-         
-//             }
-         
-//                var titleforcheck = `${doc.data().name}`;
-//                // console.log(titleforcheck);
-//                var yearforcheck = `${doc.data().years}`;
-//                // console.log(yearforcheck);
-         
-//                var regexNumber = /\d/;
-//                var regexLetter = /[a-zA-z]/;
-         
-//                if (regexLetter.test(search_input)) {
-         
-//                  if (titleforcheck.indexOf(search_input) != -1) {
-//                    var row = `
-//                    <ons-row style="margin: 5px;">
-//                    <ons-col class="text-center">
-//                    <img src="${doc.data().poster}" width="70%" style="margin: 5px 5px;" alt="">
-//                    </ons-col>
-           
-//                    <ons-col>
-//                    <p style="font-size:18px">${doc.data().name} (${doc.data().years}) </p>
-//                    <p style="font-size:15px">${doc.data().plot}</p>
-//                    <p style="font-size:15px">`+ star +`</p>
-//                    </ons-col>
-                 
-//                    </ons-row>`
-         
-         
-//                  }
-         
-         
-//                } else if (regexNumber.test(search_input)) {
-         
-//                  if (search_input == yearforcheck) {
-//                    var row = `
-//                    <ons-row style="margin: 5px;">
-//                    <ons-col class="text-center">
-//                    <img src="${doc.data().poster}" width="80%" style="margin: 5px 5px;" alt="">
-//                    </ons-col>
-           
-//                    <ons-col>
-//                    <p style="font-size:10px">${doc.data().name} (${doc.data().years}) (${doc.data().years})</p>
-//                    </ons-col>
-                 
-//                    </ons-row>`
-         
-//                  }
-            
-//                } else {
-//                  $("#search_show").empty();
-//                  $("#search_show").append("");
-         
-//                }
-         
-//                $("#search_show").append(row);
-         
-//              });
-         
-//            });
-         
-//          });
-
-//         }
-        
 window.fn = {};
 
   window.fn.open = function() {
