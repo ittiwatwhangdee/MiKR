0db693488a4108a69eca2aef9d9b0de615273b87
fhutzchx <fahut.ittiwat@gmail.com>
2020-12-04 00:44:41 +0700

    add search function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-$(function() {
+
 var firebaseConfig = {
-  apiKey: "AIzaSyAzvJ9RdYPrw_qrNCsMVpGi52c4Iqu6aao",
-  authDomain: "mikr-app.firebaseapp.com",
-  databaseURL: "https://mikr-app.firebaseio.com",
-  projectId: "mikr-app",
-  storageBucket: "mikr-app.appspot.com",
-  messagingSenderId: "314836526430",
-  appId: "1:314836526430:web:97e4577d431c7e23b79bcf",
-  measurementId: "G-ZZ5FHQJFQG"
+  apiKey: "AIzaSyDfhrdduWlyXKdyA4DOgfQYZQoJDG4qPHM",
+  authDomain: "mikr-87f57.firebaseapp.com",
+  projectId: "mikr-87f57",
+  storageBucket: "mikr-87f57.appspot.com",
+  messagingSenderId: "429837689513",
+  appId: "1:429837689513:web:14c3b4b988a2f87f32bfb8",
+  measurementId: "G-W38T1G7R57"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-})
+var db = firebase.firestore();
+
 
 $(function() {
     document.addEventListener('init', function(event) {
@@ -53,77 +53,65 @@ function openproductDetails(id) {
     });
   }
 
+
  function getfromSearch() {
 
   const searchText = $("#searchInput").val()
   const newsearchText = searchText.replace(/ /g, "");
 
   $("#searchItem").empty();
-  db.collection("product").get().then((querySnapshot) => {
+  db.collection("search").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
 
       const productname = doc.data().name;
       const newproductname = productname.replace(/ /g, "");
       console.log(newproductname);
       if (newproductname.toLowerCase().indexOf(newsearchText.toLowerCase()) != -1) {
-        if (doc.data().rating > 9) {
-          var rating = `
-          <div class="starrate">
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-    
-           <b style="font-size:x-large;color:green;text-align: right;">${doc.data().price}</b>
-          `
+        if (`${doc.data().star}`== 5) {
+          var star = `
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>`
+          
         }
-        else if (doc.data().rating >= 7 && doc.data().rating < 9) {
-          var rating = `
-          <div class="starrate">
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-    
-          <b style="font-size:x-large;color:green;text-align:right;">${doc.data().price}</b>
-          `
+        else if (`${doc.data().star}`== 4) {
+          var star = `
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>`
         }
 
-        else if (doc.data().rating >= 4 && doc.data().rating < 7) {
-          var rating = `
-          <div class="starrate">
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-     
-    
-          <b style="font-size:x-large;color:orange;text-align:right;">${doc.data().price}</b>
-          `
+        else if (`${doc.data().star}` == 3) {
+          var star = `
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>`
         }
 
-        else if (doc.data().rating > 1 && doc.data().rating < 5) {
-          var rating = `
-          <div class="starrate">
-          <ons-icon  icon="fa-star"></ons-icon>
-          <ons-icon  icon="fa-star"></ons-icon>
-            <b style="font-size:x-large;color:red;text-align:right;">${doc.data().price}</b>
-          `
-        }
+        else if (`${doc.data().star}` == 2) {
+          var star = `
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>
+                <ons-icon style="color: #EC1D8F" icon="fa-star"></ons-icon>`
+        }else{
+          var star = `
+          <ons-icon style="color: red" icon="fa-star"></ons-icon>`
+   
+      }
 
         const eiei = `
         <ons-row class="rowmagin se" id="${doc.data().name}"> 
+        <ons-row style="margin: 5px;">
+            <ons-col class="text-center">
+            <img src="${doc.data().poster}" width="80%" style="margin: 5px 5px;" alt="">
+            </ons-col>
         <ons-col>
-        <img src="${doc.data().posterURL}" style="width:90%;height:95%;">
-           </div>
-        </ons-col>
-        <ons-col>
-              <span class="list-item__title" style="font-family: 'Bebas Neue', cursive;font-size:18px;">${doc.data().name}</span><br>
-              <span class="list-item__subtitle" style="font-family: 'Open Sans', sans-serif;font-size:12px;">"${doc.data().entertainment}"
-                </span>
-                 <p class="rating">Ratings</p>
-                 + rating + 
-              </ons-col>
+            <p style="font-size:18px">${doc.data().name} (${doc.data().years}) </p>
+            <p style="font-size:15px">${doc.data().plot}</p>
+            <p style="font-size:15px">`+ star +`</p>
+            </ons-col>
       </ons-row>`
         console.log(doc.data());
         $("#searchItem").append(eiei)
@@ -137,7 +125,108 @@ function openproductDetails(id) {
 
 }
 
+// function getfromSearch() {
+           
+//             $("#search").click(function () {
+//            $("#search_show").empty();
+//            $("#sug_show").empty();
+//            $("#search_show").append("");
+//            var search_input = document.getElementById("search_input").value;
+//            console.log(search_input);
+         
+//            db.collection("search").get().then((querySnapshot) => {
+         
+//              querySnapshot.forEach((doc) => {
+//             if (`${doc.data().star}`>4){
+//                 var star = `
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
+//             }else if(`${doc.data().star}`>3){
+//                 var star = `
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
+//             }else if(`${doc.data().star}`>2){
+//                 var star = `
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
+//             }else if(`${doc.data().star}`>1){
+//                 var star = `
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
+//             }else{
+//                 var star = `
+//                 <ons-icon style="color: red" icon="fa-star"></ons-icon>`
+         
+//             }
+         
+//                var titleforcheck = `${doc.data().name}`;
+//                // console.log(titleforcheck);
+//                var yearforcheck = `${doc.data().years}`;
+//                // console.log(yearforcheck);
+         
+//                var regexNumber = /\d/;
+//                var regexLetter = /[a-zA-z]/;
+         
+//                if (regexLetter.test(search_input)) {
+         
+//                  if (titleforcheck.indexOf(search_input) != -1) {
+//                    var row = `
+//                    <ons-row style="margin: 5px;">
+//                    <ons-col class="text-center">
+//                    <img src="${doc.data().poster}" width="70%" style="margin: 5px 5px;" alt="">
+//                    </ons-col>
+           
+//                    <ons-col>
+//                    <p style="font-size:18px">${doc.data().name} (${doc.data().years}) </p>
+//                    <p style="font-size:15px">${doc.data().plot}</p>
+//                    <p style="font-size:15px">`+ star +`</p>
+//                    </ons-col>
+                 
+//                    </ons-row>`
+         
+         
+//                  }
+         
+         
+//                } else if (regexNumber.test(search_input)) {
+         
+//                  if (search_input == yearforcheck) {
+//                    var row = `
+//                    <ons-row style="margin: 5px;">
+//                    <ons-col class="text-center">
+//                    <img src="${doc.data().poster}" width="80%" style="margin: 5px 5px;" alt="">
+//                    </ons-col>
+           
+//                    <ons-col>
+//                    <p style="font-size:10px">${doc.data().name} (${doc.data().years}) (${doc.data().years})</p>
+//                    </ons-col>
+                 
+//                    </ons-row>`
+         
+//                  }
+            
+//                } else {
+//                  $("#search_show").empty();
+//                  $("#search_show").append("");
+         
+//                }
+         
+//                $("#search_show").append(row);
+         
+//              });
+         
+//            });
+         
+//          });
 
+//         }
+        
 window.fn = {};
 
   window.fn.open = function() {
